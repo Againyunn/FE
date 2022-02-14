b8abef6c4b2910b2a56c10e85d27d935eeea70af
Jaeyun Jung <rangyun36@gmail.com>
2022-02-15 01:11:46 +0900

    22.02.15 React Study

---
+++ REACT공부/React 라이브러리/date/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MomentExample from './components/MomentExample';
+
+function App() {
+  return (
+    <MomentExample />
+  );
+}
+
+export default App;
