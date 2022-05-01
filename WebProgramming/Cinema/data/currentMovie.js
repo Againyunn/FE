d73f5643db0a1e65a1f72ea63be85d4d4b35ac8a
Jaeyun Jung <rangyun36@gmail.com>
2022-05-01 19:34:37 +0900

    Create: frames in mid assignment

---
+++ WebProgramming/Cinema/data/currentMovie.js
@@ -0,0 +1,10 @@
+//현재 상영작
+let current = [
+    {
+        id: 1,
+        name: "닥터스트레인지: 대혼돈의 멀티버스",
+        time: [
+            "mon 1","tue 5","wed 3 ","thu 5","fri 4","sat 3","sun 3"
+        ]
+    }
+]
