4a967a799cab4a54eecaf32466b72871711df31e
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2022-08-19 21:03:36 +0900

    vue-study : DataBinding part1

--- vue-study/vue-project/src/views/BasicTemplate.vue
+++ vue-study/vue-project/src/views/BasicTemplate.vue
@@ -11,10 +11,37 @@
                 sampleData: ''
             };
         },
-        setup() {}, //컴포지션 API
-        created() {}, //컴포넌트가 생성
-        mouted() {}, //template에 정의된 html 코드가 랜더링된 후 실행
-        unmounted() {}, //umount가 완료된 후 실행
-        methods: {} //컴포넌트 내에서 사용할 메소드 정의
+        setup() {
+
+        }, //컴포지션 API
+        created() {
+
+        }, //컴포넌트가 생성
+        mouted() {
+
+        }, //template에 정의된 html 코드가 랜더링된 후 실행
+        unmounted() {
+
+        }, //umount가 완료된 후 실행
+        methods: {
+            
+        } //컴포넌트 내에서 사용할 메소드 정의
     }
 </script>
+
+
+<!-- VS Code의 단축키로 설정 -->
+<!-- 설정 경로:
+window 기준[
+    파일 -> 기본설정 -> 사용자 코드 조각 구성 -> vue 언어 선택 후 vue.json 에 하기 내용 복붙
+] -->
+
+<!-- {
+"Generate Basic Vue Code" : {
+		"prefix": "vue-start",
+		"body": [
+			"<template>\n\t<div></div>\n</template>\n<script>\nexport default{\n\t//컴포넌트 이름\n\tname: '',\n\t//다른 컴포넌트 사용 시 컴포넌트를 import하고, 배열로 저장\n\tcomponents:{\n\n\t\t}, \n\t//html과 JS코드에서 사용할 데이터 변수 선언\n\tdata(){ \n\t\treturn{\n\t\t\tsampleData: ''\n\t\t};\n\t},\n\t//컴포지션 API\n\tsetup() {\n\t\t\n\t},\n\t//컴포넌트 생성\n\tcreated() {\n\t\t\n\t},\n\t//template에 정의된 html 코드가 랜더링된 후 실행\n\tmouted() {\n\t\t\n\t},\n\t//umount가 완료된 후 실행\n\tunmounted() {\n\t\t\n\t},\n\t//컴포넌트 내에서 사용할 메소드 정의\n\tmethods: {\n\t\t\n\t}\n}\n</script>"
+		],
+		"description": "Generate Basic Vue Code"
+	}
+} -->
---
+++ vue-study/vue-project/src/views/DataBinding.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div>
+            <h3>html바인딩 테스트</h3>
+            <div>{{htmlString}}</div>
+            <div v-html="htmlString"></div>
+        </div>
+        <div>
+            <h3>From 입력 데이터 바인딩 테스트</h3>
+            <label for="t-text">문자열:&nbsp;</label>
+            <input id="t-text" type="text" v-model="valueModel"/>
+            <br/>
+            <label for="t-number">숫자:&nbsp;</label>
+            <input id="t-number" type="number" v-model="numberModel.number"/>            
+        </div>
+        <div>
+            <h3>Textarea사용</h3>
+            <textarea v-model="message"></textarea>
+        </div>
+        <div>
+            <h3>select 객체 사용</h3>
+            <select v-model="city">
+                <option value="02">서울</option>
+                <option value="21">부산</option>
+                <option value="064">제주</option>
+            </select>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default{
+    //html과 JS코드에서 사용할 데이터 변수 선언
+    data(){ 
+        return{
+            title: 'World',
+            htmlString: '<span style="color:red;">This is a red String</span>',
+            valueModel: 'South Korea',
+            numberModel: 3,
+            message: "여러 줄을 입력할 수 있는 textarea입니다.\n본 글은 미리 표시되는 글입니다.",
+            city: "064"
+        };
+    }
+}
+</script>
