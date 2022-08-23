a1dcc956eb18974802b42be2f859bcfa31a1da8b
Jaeyun Jung <76543591+Againyunn@users.noreply.github.com>
2022-08-23 15:15:11 +0900

    vue-study : set Proxy

--- vue-study/vue-project/vue.config.js
+++ vue-study/vue-project/vue.config.js
@@ -8,3 +8,19 @@ module.exports = {
     config.plugins.delete('prefetch'); //prefetch 삭제
   }
 }
+
+/**proxy(프록시) 설정 */
+const target = 'http:127.0.0.1:3000';
+
+module.exports = {
+  devServer: {
+    port: 8080,
+    proxy:{
+      //proxy요청을 보낼 API 시작 부분
+      '^/api':{
+        target,
+        changeOrigin: true
+      }
+    }
+  }
+}
